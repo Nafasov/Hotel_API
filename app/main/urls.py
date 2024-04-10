e6a105566d14b105ced42de08c4c79cc9332ae0d
Nafasov <nafasovnurxon18@gmail.com>
2024-04-10 13:07:38 +0500

    modified

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
 
+from .views import (
+    AboutUsAPIView,
+    MeContactAPIView,
+    ServiceAPIView,
+    PartnersAPIView,
+    HomeBannerAPIView
+    )
+
 
 app_name = 'main'
 
 urlpatterns = [
-
+    path('hom/baner/', HomeBannerAPIView.as_view(), name='home'),
+    path('about/', AboutUsAPIView.as_view(), name='about'),
+    path('service/', ServiceAPIView.as_view(), name='service'),
+    path('partners/', PartnersAPIView.as_view(), name='partners'),
+    path('mecontact/', MeContactAPIView.as_view(), name='me_contact'),
 ]
 
 
